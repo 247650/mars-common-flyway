1da14a2c1158bc0256246eeb39badde1c77f3944
MAXCORP\202234 <rakesh.gupta@maximus.com>
2019-04-10 14:32:34 -0500

    CRM-2273 Sample test

--- src/test/java/com/maximus/mars/common/flyway/service/EnableMultiTenancyFlywayMigrationTest.java
+++ src/test/java/com/maximus/mars/common/flyway/service/EnableMultiTenancyFlywayMigrationTest.java
@@ -1,6 +1,13 @@
 package com.maximus.mars.common.flyway.service;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@EnableMultiTenancyFlywayMigration
 public class EnableMultiTenancyFlywayMigrationTest {
 
+	@Test
+	void testMigrate() {
+	}	
+	
 }
