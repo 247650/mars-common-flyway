36a5ad6bd7e675c576f7d1d0cb9216269b9ec4cc
Rafi Mohammed/MAXIMUS <rafimohammed@maximus.com>
2019-04-25 12:24:30 +0530

    adding unit test

--- src/test/java/com/maximus/mars/common/flyway/service/EnableMultiTenancyFlywayMigrationTest.java
+++ src/test/java/com/maximus/mars/common/flyway/service/EnableMultiTenancyFlywayMigrationTest.java
@@ -2,12 +2,24 @@ package com.maximus.mars.common.flyway.service;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 @EnableMultiTenancyFlywayMigration
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@DataJpaTest
+@TestPropertySource(
+		locations = "classpath:application-test.properties")
+@ContextConfiguration(loader=AnnotationConfigContextLoader.class)
 public class EnableMultiTenancyFlywayMigrationTest {
 
 	@Test
-	void testMigrate() {
+	public void testMigrate() {
 	}	
 	
 }
