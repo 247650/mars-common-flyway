1844a99354caf1cf19fcf1c78ed4757caa631803
Vivek Jaiswal/MAXIMUS <vivekjaiswal@maximus.com>
2019-05-03 18:45:34 +0530

    changes commit

--- src/main/java/com/maximus/mars/common/flyway/service/FlywayService.java
+++ src/main/java/com/maximus/mars/common/flyway/service/FlywayService.java
@@ -1,20 +1,17 @@
 /**
  * 
  */
-package com.maximus.mars.common.flyway.service;
-
-import org.springframework.stereotype.Component;
-
-/**
- * @author 80759
- *
- */
-@Component
-public class FlywayService 
-{
-	@EnableMultiTenancyFlywayMigration
-	public String welcome() 
-	{
-	    return "welcome";
-	}
-}
+/*
+ * package com.maximus.mars.common.flyway.service;
+ * 
+ * import org.springframework.stereotype.Component;
+ * 
+ *//**
+	 * @author 80759
+	 *
+	 *//*
+		 * @Component public class FlywayService {
+		 * 
+		 * @EnableMultiTenancyFlywayMigration public String welcome() { return
+		 * "welcome"; } }
+		 */
