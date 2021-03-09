cd0bdccf750c867572fa6f452ada236215ab19b3
Suman Polimetla <sumanbabupolimetla@max.com>
2021-03-09 16:14:30 -0600

    CP-21032 Add out of order support with default as false

--- src/main/java/com/maximus/mars/common/flyway/service/EnableMultiTenancyFlywayMigrationImpl.java
+++ src/main/java/com/maximus/mars/common/flyway/service/EnableMultiTenancyFlywayMigrationImpl.java
@@ -1,12 +1,10 @@
 package com.maximus.mars.common.flyway.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.flywaydb.core.Flyway;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -22,12 +20,15 @@ public class EnableMultiTenancyFlywayMigrationImpl {
 	private static final Logger logger = LoggerFactory.getLogger(EnableMultiTenancyFlywayMigrationImpl.class);
 
 	private MultitenantProperties multitenantProperties;
-	
+
 	private CredentialsService credentialService;
+
+	@Value("${mars.flyway.outOfOrder:false}")
+	private Boolean outOfOrder;
 
 	@Autowired
 	public void setMultitenancyProperties(MultitenantProperties multitenantProperties,
-			CredentialsService credentialService) {
+										  CredentialsService credentialService) {
 		this.multitenantProperties = multitenantProperties;
 		this.credentialService = credentialService;
 	}
@@ -35,40 +36,31 @@ public class EnableMultiTenancyFlywayMigrationImpl {
 	public boolean migrate() {
 		multitenantProperties.getDatasources().stream().forEach(dataSource -> {
 			try {
-				
-				
+				logger.info("Starting flyway migration for locations" + dataSource.getLocation());
+
 				if(dataSource.getSecret() != null ) {
-					logger.info("getting the password from credential service using key "+dataSource.getSecret());
+					logger.info("getting the password from credential service	 using key "+dataSource.getSecret());
 					MarsDBSecret marsDBSecret =  credentialService.getCachedSecret(dataSource.getSecret(),false, MarsDBSecret.class);
 					dataSource.setUsername(marsDBSecret.getUsername());
-					dataSource.setPassword(marsDBSecret.getPassword());					
+					dataSource.setPassword(marsDBSecret.getPassword());
 				}else {
 					dataSource.setUsername(dataSource.getUsername());
-					dataSource.setPassword(dataSource.getPassword());	
+					dataSource.setPassword(dataSource.getPassword());
 				}
-				
-				List<String> locations = new ArrayList<String>();
-				if(dataSource.getLocation() != null) {
-					locations.add(dataSource.getLocation());
-				}
-				
-				if(dataSource.getLocations() != null && dataSource.getLocations().size() > 0) {
-					locations.addAll(dataSource.getLocations());
-				}
-				
-				logger.info("Starting flyway migration for locations {}" , locations);
-				
+
 				Flyway flyway = Flyway.configure()
 						.dataSource(dataSource.getUrl(), dataSource.getUsername(), dataSource.getPassword())
-						.baselineOnMigrate(true).locations(locations.toArray(new String[0])).load();
+						.baselineOnMigrate(true)
+						.outOfOrder(outOfOrder.booleanValue())
+						.locations(dataSource.getLocation()).load();
 				logger.info("Performing repair ");
 				flyway.repair();
 				logger.info("Migrating  ");
 				flyway.migrate();
-				logger.info("Flyway migration information {}" , flyway.info());
-				logger.info("Finished flyway migration for locations {}" ,locations);
+				logger.info("Flyway migration information " + flyway.info());
+				logger.info("Finished flyway migration for locations" + dataSource.getLocation());
 			} catch (Exception e) {
-				logger.error("Error flyway migration" ,e);
+				logger.error("Error flyway migration for locations :" + dataSource.getLocation(),e);
 			}
 		});
 		return true;
